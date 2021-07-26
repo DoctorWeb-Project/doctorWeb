65a0ae49c19b90948bf59693e0a9eb67f5a7b4b7
Ed-Games <edmarcosfilho2015@gmail.com>
2021-07-26 17:31:23 -0300

    fix: spelling of database name

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
       host : '127.0.0.1',
       user : 'root',
       password : '',
-      database : 'doctorWeb'
+      database : 'doctorweb'
     },
     migrations: {
       tableName: 'knex_migrations',
