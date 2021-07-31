a8b1f0883198b64f18e59f50298ffa6a0394aa54
Ed-Games <edmarcosfilho2015@gmail.com>
2021-07-31 20:08:46 -0300

    update: purpleButton can now be a link or button

--- frontend/src/components/PurpleButton/index.js
+++ frontend/src/components/PurpleButton/index.js
@@ -4,7 +4,14 @@ import {Link} from 'react-router-dom'
 import {FiLogIn} from 'react-icons/fi'
 
 export function PurpleButton(props){
-    return(
+    return props.element === "button"? (
+        <button className="purpleButton" type={props.type} to={props.navigateTo}>
+            <div className="loginIcon">
+                <FiLogIn size={24} color="#FFF"/>
+            </div>
+            <span>{props.title}</span>
+        </button>
+    ) : (
         <Link className="purpleButton" to={props.navigateTo}>
             <div className="loginIcon">
                 <FiLogIn size={24} color="#FFF"/>
