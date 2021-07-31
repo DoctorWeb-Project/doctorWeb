848977d1b5d8c01c934769dfff276c2534b7cf49
Ed-Games <edmarcosfilho2015@gmail.com>
2021-07-31 18:03:18 -0300

    update: added Register and Planner routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,12 +2,16 @@ import {BrowserRouter, Route} from 'react-router-dom'
 import React from 'react'
 import {Home} from './pages/Home'
 import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { Planner } from './pages/Planner'
 
 export function Routes(){
     return(
         <BrowserRouter>
             <Route component={Home} path="/" exact />
             <Route component={Login} path="/login" exact />
+            <Route component={Register} path="/register" exact />
+            <Route component={Planner} path="/planner" exact />
         </BrowserRouter>
     )
 }
