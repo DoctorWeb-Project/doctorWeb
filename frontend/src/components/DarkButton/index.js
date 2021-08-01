c62be0850987bf476851bc10d4cece79950015e2
Ed-Games <edmarcosfilho2015@gmail.com>
2021-08-01 09:55:01 -0300

    update: added action prop to handle click events

--- frontend/src/components/DarkButton/index.js
+++ frontend/src/components/DarkButton/index.js
@@ -3,6 +3,6 @@ import './styles.css'
 
 export function DarkButton(props){
     return(
-        <button className="darkButton" type={props.type}>{props.title}</button>
+        <button className="darkButton" type={props.type} onClick={props.action}>{props.title}</button>
     )
 }
