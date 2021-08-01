f667619733d80912d1179e8bd631469d62c7f5df
Ed-Games <edmarcosfilho2015@gmail.com>
2021-07-31 22:25:49 -0300

    update: created signIn function to log the user in the API

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
 import { PurpleButton } from '../../components/PurpleButton'
+import { useUser } from '../../contexts/userContext'
+import api from '../../services/api'
 
 import './styles.css'
 
 export function Login(){
+    const [cpf, setCPF] = useState('')
+    const [password, setPassword] = useState('')
+    const {saveUserTokenToStorage} = useUser()
+    const history = useHistory()
+
+    async function signIn(event){
+        event.preventDefault()
+        try{
+            const response = await api.post('/login', {cpf: cpf, senha: password})
+            saveUserTokenToStorage(response)
+            console.log(response)
+            history.push('/planner')
+
+        } catch (error) {
+            console.log(error)
+            
+        }
+        
+    }
+
     return(
         <div className="loginContainer">
             <header>
@@ -16,10 +38,10 @@ export function Login(){
                 <h1>Otimiza seu tempo e cuida da sua saúde</h1>
                 <p>Ajudamos pessoas a organizarem suas consultas médicas.</p>
 
-                <form>
-                    <input placeholder="Nome de usuário" />
-                    <input placeholder="Senha" />
-                    <PurpleButton title="Entrar" element="button" type="submit" />
+                <form onSubmit={signIn}>
+                    <input value={cpf} onChange={event => setCPF(event.target.value)} placeholder="número do cpf" />
+                    <input value={password} onChange={event=>setPassword(event.target.value)} placeholder="Senha" />
+                    <PurpleButton title="Entrar" element="button" type="submit"/>
                 </form>
 
                 <span>Não possui uma conta? <Link to="/register">Clique aqui!</Link></span>
