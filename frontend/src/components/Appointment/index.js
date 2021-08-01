3ea1d4aef38d5f19ae3320cf543c2ce725f72350
Ed-Games <edmarcosfilho2015@gmail.com>
2021-08-01 09:54:39 -0300

    update: passing real data to each appointment

--- frontend/src/components/Appointment/index.js
+++ frontend/src/components/Appointment/index.js
@@ -3,23 +3,25 @@ import {FiEdit, FiCalendar, FiClock} from 'react-icons/fi'
 
 import './styles.css'
 
-export function Appointment(){
+export function Appointment(props){
+    console.log(props)
+
     return(
         <div className="appointment">
             <div className="singleLine" />
             <div className="appointmentAction">
-                <span>Consulta ao dentista</span>
+                <span>{props.data?.nome}</span>
                 <FiEdit color="#8257E5" size={24} />
             </div>
             <div className="appointmentInfo">
                 <FiCalendar color="#9C7AE9" size={24} />
                 
-                <span className="dateSpan">24/07</span>
+                <span className="dateSpan">{props.data?.dia}/{props.data?.mes}</span>
                 <span className="pipe">|</span>
                 <FiClock color="#34CB79" size={24} />
-                <span className="timeSpan">14:10</span>
+                <span className="timeSpan">{props.data?.hora}:{props.data?.minutos}</span>
                 <span className="pipe">|</span>
-                <span className="priceSpan">R$ 250,00</span>
+                <span className="priceSpan">R$ {props.data?.preco}</span>
 
             </div>
         </div>
