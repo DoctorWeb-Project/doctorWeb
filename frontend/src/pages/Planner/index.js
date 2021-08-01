4c4f3a7a69e6f554f0d09b20f77a7b74fcebc2ea
Ed-Games <edmarcosfilho2015@gmail.com>
2021-08-01 09:56:26 -0300

    update: adding loading function to get appointments from the api

--- frontend/src/pages/Planner/index.js
+++ frontend/src/pages/Planner/index.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Appointment } from '../../components/Appointment'
 import {DarkButton} from '../../components/DarkButton'
 import Logo from '../../assets/logo.png'
 import {FiEdit} from 'react-icons/fi'
 
 import './styles.css'
+import api from '../../services/api'
+import { useHistory } from 'react-router-dom'
+
 
 export function Planner(){
+    const [appointments, setAppointments] = useState([])
+    const history = useHistory()
+    const month = new Date().getMonth() +1
+
+    async function loadAppointments(){
+        try {
+            const response =  await api.get('/minhasConsultas')
+            setAppointments(response.data)
+        } catch (error) {
+            console.log("planner error: ",error.response)
+            if(error.response && ( error.response.status === 401 || error.response.status === 403)){
+                history.push('/login')
+            }
+        }
+    }
+
+
+    useEffect(()=>{
+        loadAppointments()
+    },[])
+
     return(
         <div className="plannerContainer">
             <header>
@@ -26,24 +50,36 @@ export function Planner(){
             <main>
                 <div className="card">
                     <h1>Este mês</h1>
-                    <Appointment />
-                    <Appointment />
-                    <Appointment />
-                    <Appointment />
+                    { appointments.length>0 && appointments.map(appointment => {
+                        console.log(month)
+                        if(appointment.mes == month ){
+                            return(
+                                <Appointment data={appointment} />
+                            )
+                        }
+                    })}
                 </div>
                 <div className="card">
                     <h1>Próximo mês</h1>
-                    <Appointment />
-                    <Appointment />
-                    <Appointment />
-                    <Appointment />
+                    { appointments.length>0 && appointments.map(appointment => {
+                        console.log(month)
+                        if(appointment.mes == month + 1 ){
+                            return(
+                                <Appointment data={appointment} />
+                            )
+                        }
+                    })}
                 </div>
                 <div className="card">
                     <h1>Seguintes</h1>
-                    <Appointment />
-                    <Appointment />
-                    <Appointment />
-                    <Appointment />
+                    { appointments.length>0 && appointments.map(appointment => {
+                        console.log(month)
+                        if(appointment.mes > month + 1 ){
+                            return(
+                                <Appointment data={appointment} />
+                            )
+                        }
+                    })}
                 </div>
             </main>
 
