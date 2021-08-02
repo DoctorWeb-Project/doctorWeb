df5a3b15121e6d02135c567080020ec809edd564
Ed-Games <edmarcosfilho2015@gmail.com>
2021-08-01 23:55:28 -0300

    update: added id prop

--- frontend/src/components/DarkButton/index.js
+++ frontend/src/components/DarkButton/index.js
@@ -3,6 +3,6 @@ import './styles.css'
 
 export function DarkButton(props){
     return(
-        <button className="darkButton" type={props.type} onClick={props.action}>{props.title}</button>
+        <button id={props.id} className="darkButton" type={props.type} onClick={props.action}>{props.title}</button>
     )
 }
